9489eaf0da911404e9213598e568f7edd4de9151
Jeriel <164656772+jerielab@users.noreply.github.com>
2026-09-24 01:03:41 +0700

    proxy fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,5 +10,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {})
-    ->withExceptions(function (Exceptions $exceptions): void {})->create();
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {})
+    ->create();
